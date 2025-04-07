c72fe9254b33359ac90d13581679f5cffb6a65fa
Huy Ton <huyton2203@gmail.com>
2025-04-07 15:09:48 +0700

    no message

--- test_train.py
+++ test_train.py
@@ -34,6 +34,7 @@ for epoch in range(10):
         disc_total_loss = 0
         inputs = (torch.randn(BATCH_SIZE, 3, 256, 256).to(device), torch.randn(BATCH_SIZE, 1, 256, 256).to(device))
         imgs, masks = inputs[0].to(device), inputs[1].to(device)
+        targets = torch.randn(BATCH_SIZE, 3, 256, 256).to(device)
         targets = targets.to(device)
         outputs = model(imgs, masks)
 
