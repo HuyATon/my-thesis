0e9844ea552d9a0208ca0243cda8420dc345138c
Binh0307 <146600416+Binh0307@users.noreply.github.com>
2025-04-14 15:37:12 +0700

    Update network_pro.py

--- network/network_pro.py
+++ network/network_pro.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 from network.vit import ViT
 from network.refine import Refine
-import cv2
+
 
 class Inpaint(nn.Module):
     def __init__(self, input_size=256, patch_size=16, depth=15, heads=16):
@@ -11,7 +11,7 @@ class Inpaint(nn.Module):
         self.refine = Refine(6)
 
     def forward(self, img, mask):
-        c_gen, stack = self.coarse(img * (1 - mask), mask)  # mask (0: valid, 1: hole)
+        c_gen, stack = self.coarse(img * (1 - mask), mask)
         c_gen_ = []
         for c_g in c_gen:
             c_gen_.append((c_g * mask) + img * (1 - mask))
