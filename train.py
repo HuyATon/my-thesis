29dfdf45115ec8eb9fb43cd36794f9d242e4a84b
Huy Ton <huyton2203@gmail.com>
2025-04-16 16:41:06 +0700

    no message

--- train.py
+++ train.py
@@ -1,21 +1,51 @@
-from utils_classes.train_session import TrainSession
+from train_session import TrainSession
 import torch
-from configs import *
+import argparse
 from dataset.inpainting_dataset import InpaintingDataset
 from torch.utils.data import DataLoader
 
 
+# Create argument parser for: 
+# --img_dir
+# --mask_dir
+# --batch_size
+# --model_checkpoint
+# --disc_checkpoint
+# --repo_path
+parser = argparse.ArgumentParser(description="Training parameters")
+parser.add_argument("--img_dir", type=str, required=True, help="Path to the image directory")
+parser.add_argument("--mask_dir", type=str, required=True, help="Path to the mask directory")
+parser.add_argument("--batch_size", type=int, default=8, help="Batch size for training")
+parser.add_argument("--model_checkpoint", type=str, default=None, help="Path to the model checkpoint")
+parser.add_argument("--disc_checkpoint", type=str, default=None, help="Path to the discriminator checkpoint")
+parser.add_argument("--repo_path", type=str, required=True, help="Path to save checkpoints")
+
+args = parser.parse_args()
+# Assign arguments to variables
+img_dir = args.img_dir
+mask_dir = args.mask_dir
+batch_size = args.batch_size
+model_checkpoint = args.model_checkpoint
+disc_checkpoint = args.disc_checkpoint
+repo_path = args.repo_path
+
+
+
 assert torch.cuda.is_available(), "CUDA is not available."
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")   
-train_dataset = InpaintingDataset(img_dir=IMG_DIR, mask_dir=MASK_DIR)
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE) 
+train_dataset = InpaintingDataset(img_dir=img_dir, mask_dir=mask_dir)
+train_loader = DataLoader(train_dataset, batch_size=batch_size) 
 
-train_session = TrainSession(device = device,
-                             model_checkpoint=MODEL_CHECKPOINT,
-                             disc_checkpoint=DISC_CHECKPOINT,
-                             train_loader=train_loader,
-                             checkpoint_repo=CHECKPOINTS_DIR,
-                             lr=LR
-                             )
-train_session.load_checkpoints()
-train_session.start_training()
+if __name__ == "__main__":
+    train_session = TrainSession(
+        device=device,
+        train_loader=train_loader,
+        checkpoint_repo=repo_path
+    )
+    train_session.prepare_repo()
+    if model_checkpoint and disc_checkpoint:
+        train_session.load_checkpoints(model_checkpoint, disc_checkpoint)
+        print("Checkpoints loaded successfully.")
+    else:
+        print("No checkpoints provided. Starting from scratch.")
+    train_session.start_training()
--- train_session.py
+++ train_session.py
@@ -1,6 +1,9 @@
 import torch
 import os
 import time
+import torch.nn as nn
+
+from losses.combined import CombinedLoss
 from network.network_pro import Inpaint
 from network.discriminator import Discriminator
 from utils import save_checkpoint, save_loss
@@ -14,98 +17,103 @@ OPT_KEY = "optimizer_state_dict"
 
 class TrainSession:
     def __init__(self, 
-                 device: str, 
-                 model_checkpoint: str, 
-                 disc_checkpoint: str, 
+                 device: str,
                  train_loader: DataLoader,
                  checkpoint_repo: str,
                  lr= 1e-3, 
                  duration= 48 * 60 * 60, # 2 days
                  save_window= 60 * 60 # 1 hour
                  ):
+        self.start_time = time.time()
+        self.latest_checkpoint_time = time.time()
         self.max_epoch = 9999999
-        self.current_epoch = 0
-
+        self.last_epoch = 0
         self.device = device
-        self.model_checkpoint = model_checkpoint
-        self.disc_checkpoint = disc_checkpoint
-        self.checkpoint_repo = checkpoint_repo
         self.train_loader = train_loader
+        self.checkpoint_repo = checkpoint_repo
         self.lr = lr
         self.duration = duration
         self.save_window = save_window
       
         self.model = Inpaint().to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.criterion = CombinedLoss().to(device)
+
+
         self.disc = Discriminator().to(self.device)
         self.disc_optimizer = torch.optim.Adam(self.disc.parameters(), lr=self.lr)
+        self.disc_criterion = nn.BCEWithLogitsLoss().to(device)
 
+
+    def prepare_repo(self):
+        for repo in ["loss", "model", "disc"]:
+            repo_path = os.path.join(self.checkpoint_repo, repo)
+            if not os.path.exists(repo_path):
+                os.makedirs(repo_path)
     
-    def load_checkpoints(self):
-        if self.model_checkpoint != None and self.disc_checkpoint != None:
-            saved_checkpoint = torch.load(self.model_checkpoint, map_location=self.device)
-            self.model.load_state_dict(saved_checkpoint[WEIGHT_KEY])
-            self.optimizer.load_state_dict(saved_checkpoint[OPT_KEY])
-            saved_checkpoint = torch.load(self.disc_checkpoint, map_location=self.device)
-            self.disc.load_state_dict(saved_checkpoint[WEIGHT_KEY])
-            self.disc_optimizer.load_state_dict(saved_checkpoint[OPT_KEY])
-            self.current_epoch = saved_checkpoint[EPOCH_KEY]  +  1
-            saved_checkpoint = None # free memory
+    def load_checkpoints(self, model_path: str, disc_path: str):
+        saved_checkpoint = torch.load(model_path, map_location=self.device)
+        self.model.load_state_dict(saved_checkpoint[WEIGHT_KEY])
+        self.optimizer.load_state_dict(saved_checkpoint[OPT_KEY])
+        saved_checkpoint = torch.load(disc_path, map_location=self.device)
+        self.disc.load_state_dict(saved_checkpoint[WEIGHT_KEY])
+        self.disc_optimizer.load_state_dict(saved_checkpoint[OPT_KEY])
 
-    def save_checkpoint(self, epoch):
-        model_checkpoint_dest = os.path.join(self.checkpoint_repo, f'model_{epoch}.pth')
-        disc_checkpoint_dest = os.path.join(self.checkpoint_repo, f'disc_{epoch}.pth')
-        save_checkpoint(model_checkpoint_dest, epoch, self.model, self.optimizer)
-        save_checkpoint(disc_checkpoint_dest, epoch, self.disc, self.disc_optimizer)
-    
-    def save_loss(self, epoch, model_loss, disc_loss):
-        loss_checkpoint_dest = os.path.join(self.checkpoint_repo, f'loss_{epoch}.pth')
-        save_loss(loss_checkpoint_dest, epoch, model_loss, disc_loss)
-
+        self.current_epoch = saved_checkpoint[EPOCH_KEY] + 1
 
     def start_training(self):
-        start_time = time.time()
-        lastest_checkpoint_time = time.time()
+        self.prepare_repo()
         self.model.train()
         self.disc.train()
-        for epoch in range(self.current_epoch, self.max_epoch):
-            progress = time.time() - start_time
+        for epoch in range(self.last_epoch, self.max_epoch):
+            progress = time.time() - self.start_time
             if progress > self.duration:
-                return
+                return # stop training
             model_total_loss = 0
             disc_total_loss = 0
-            for batch_idx, (inputs, targets) in enumerate(self.train_loader):
+            for (inputs, targets) in enumerate(self.train_loader):
                 imgs, masks = inputs[0].to(self.device), inputs[1].to(self.device)
                 targets = targets.to(self.device)
-                outputs = self.model(imgs, masks)
+
+                fakes = self.model(imgs, masks)
 
                 # Train Discriminator
                 self.disc_optimizer.zero_grad()
-                disc_fake_pred = self.disc(outputs.to(self.device))
-                disc_real_pred = self.disc(targets.to(self.device))
-                disc_fake_loss = self.disc_criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred).to(self.device))
-                disc_real_loss = self.disc_criterion(disc_real_pred, torch.ones_like(disc_real_pred).to(self.device))
-                disc_loss = (disc_fake_loss + disc_real_loss) / 2
+                fake_pred = self.disc(fakes.detach())
+                real_pred = self.disc(targets)
+                fake_loss = self.disc_criterion(fake_pred, torch.zeros_like(fake_pred))
+                real_loss = self.disc_criterion(real_pred, torch.ones_like(real_pred))
+                disc_loss = (fake_loss + real_loss) / 2
                 disc_total_loss += disc_loss.item()
                 disc_loss.backward()
                 self.disc_optimizer.step()
 
                 # Train Model
                 self.optimizer.zero_grad()
-                outputs = self.model(imgs, masks)
-                disc_fake_pred = self.disc(outputs.to(self.device))
-                disc_loss = self.disc_criterion(disc_fake_pred, torch.ones_like(disc_fake_pred).to(self.device))
-                loss = self.criterion(masks.to(self.device), outputs.to(self.device), targets.to(self.device), disc_loss)
+                fake_pred = self.disc(fakes)
+                disc_loss = self.disc_criterion(fake_pred, torch.ones_like(fake_pred))
+                loss = self.criterion(masks, fakes, targets, disc_loss)
                 model_total_loss += loss.item()
                 loss.backward()
                 self.optimizer.step()
 
-            self.save_loss(epoch = epoch, 
-                        model_loss = model_total_loss / len(self.train_loader),
-                        disc_loss = disc_total_loss / len(self.train_loader))
-            if time.time() - lastest_checkpoint_time > self.save_window:
-                self.save_checkpoint(epoch=epoch)
-                lastest_checkpoint_time = time.time()
-            self.current_epoch += 1
+            self.perform_logging(epoch, 
+                                 model_total_loss / len(self.train_loader), 
+                                 disc_total_loss / len(self.train_loader))
 
+    def perform_logging(self, epoch: int, model_loss: float, disc_loss: float):
+        self.log_loss(epoch, model_loss, disc_loss)
+        if time.time() - self.latest_checkpoint_time > self.save_window and time.time() - self.start_time > 24 * 60 * 60:
+            self.latest_checkpoint_time = time.time()
+            self.log_checkpoint(epoch=epoch)
+
+    def log_checkpoint(self, epoch: int):
+        model_checkpoint_dest = os.path.join(self.checkpoint_repo, 'model', f'model_{epoch}.pth')
+        disc_checkpoint_dest = os.path.join(self.checkpoint_repo, 'disc', f'disc_{epoch}.pth')
+        save_checkpoint(model_checkpoint_dest, epoch, self.model, self.optimizer)
+        save_checkpoint(disc_checkpoint_dest, epoch, self.disc, self.disc_optimizer)
+    
+    def log_loss(self, epoch: int, model_loss: float, disc_loss: float):
+        loss_checkpoint_dest = os.path.join(self.checkpoint_repo, 'loss' ,f'loss_{str(epoch).zfill(4)}.pth')
+        save_loss(loss_checkpoint_dest, epoch, model_loss, disc_loss)
   
